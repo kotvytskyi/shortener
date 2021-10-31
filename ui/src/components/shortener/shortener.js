edab4ad01faf33bc693f860852d532abcb098f64
kotvytskyi <oleksandr.kotvytskyi@gmail.com>
2021-10-31 20:55:30 +0200

    feat: add validation

--- ui/src/components/shortener/shortener.js
+++ ui/src/components/shortener/shortener.js
@@ -3,18 +3,52 @@ import { Input } from '../input/input'
 import { ShortInput } from '../shortInput/shortInput'
 import { Button } from '../button/button';
 import ShortApi from '../../api/shortAPI';
+import validator from 'validator';
 
 import './shortener.css'
 
 export const Shortener = () => {
-    const [longURI, setLongUri] = useState(null)
+    const [uri, setUri] = useState(null);
+    const [uriValid, setUriValid] = useState(false)
+
+    const [uriErrorMessage, setUriErrorMessage] = useState(null)
+    const [shortErrorMessage, setShortErrorMessage] = useState(null)
+
     const [short, setShort] = useState(null)
+    const [shortValid, setShortValid] = useState(false)
+
+    const handleUriChange= (input) => {
+        setUri(input.target.value);
+    }
 
     const handleGenerateShort = (longURI, short) => {
-        console.log(longURI, short);
         ShortApi
             .generateShort(longURI, short)
             .then(generatedShort => setShort(generatedShort))
+    }
+
+    const validateUri = uri => ({
+        valid: uri && validator.isURL(uri),
+        message: "URL is invalid"
+    })
+    const validateShort = short => ({
+        valid: short && validator.isLength(short, { min: 3, max: 10 }),
+        message: "Short length must be between 3 and 10 characters"
+    })
+
+    const handleSubmit = (_) => {
+        const uriValidation = validateUri(uri)
+        const shortValidation = validateShort(short)
+
+        setUriValid(uriValidation.valid)
+        setUriErrorMessage(uriValidation.message)
+
+        setShortValid(shortValidation.valid)
+        setShortErrorMessage(shortValidation.message)
+
+        if (uriValid && shortValid) {
+            handleGenerateShort(uri, short)
+        }
     }
 
     return (
@@ -22,16 +56,28 @@ export const Shortener = () => {
             <p>
                 <b>As a</b> smart ass
             </p>
-            <p>
-                <b>I want</b> <Input onChange={e => setLongUri(e.target.value)} className="long" placeholder="this long url" autoFocus/>
-            </p>
-            <p>
+            <div className="row">
+                <b>I want </b> 
+                <Input 
+                    isInvalid={!uriValid}
+                    errorMessage={uriErrorMessage}
+                    onChange={handleUriChange}
+                    className="long"
+                    placeholder="this long url"
+                    autoFocus/>
+            </div>
+            <div className="row">
                 <span>To be short </span>  
-                <ShortInput domain="https://shortener.com/" className="short" onChange={e => setShort(e.target.value)} />
-            </p>
+                <ShortInput
+                    domain="https://shortener.com/"
+                    className="short"
+                    isInvalid={!shortValid}
+                    errorMessage={shortErrorMessage}
+                    onChange={e => setShort(e.target.value)} />
+            </div>
             <p>
                 <b>So that</b> it looks fancy. 
             </p>
-            <Button text="Save" onClick={() => handleGenerateShort(longURI, short)}/>
+            <Button text="Save" onClick={handleSubmit}/>
         </section>)
 }
